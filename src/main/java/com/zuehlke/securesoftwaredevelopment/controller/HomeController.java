2669877391ebfe4e45658c3d9432b69810900c6b
Luka Sotra <lukasotra@gmail.com>
2025-10-04 03:08:01 +0200

    feat: add `redirect` http endpoint

--- src/main/java/com/zuehlke/securesoftwaredevelopment/controller/HomeController.java
+++ src/main/java/com/zuehlke/securesoftwaredevelopment/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.zuehlke.securesoftwaredevelopment.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -9,5 +10,10 @@ public class HomeController {
     @GetMapping("/home")
     public String showHome() {
         return "home";
+    }
+
+    @GetMapping("/redirect")
+    public String redirectTo(@RequestParam("url") String url) {
+        return "redirect:" + url;
     }
 }
